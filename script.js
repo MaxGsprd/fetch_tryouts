5c5ea830ae18f8efe7c03b3e89e3bb90fc51ac7a
MaxGsprd <max.gaspard@outlook.fr>
2020-09-07 21:59:29 +0200

    fecth post added in script.js

--- script.js
+++ script.js
@@ -13,6 +13,24 @@ const getUsers = async function () {
         console.log(e);
     }
 }
+
 getUsers();
 
+// fecht en post
 
+const insertPost = async function (data) {
+    let response = await fetch('http://jsonplaceholder.typicode.com/posts',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    });
+    if (responseData.ok) {
+        let data = await response.json();
+        console.log(responseData);
+    }
+}
+
+insertPost({
+    name: 'Jean',
+    age: 29
+});
